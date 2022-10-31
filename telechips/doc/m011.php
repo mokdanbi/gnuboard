61541b7b020948910bfb5084d5f8d758f1908928
mokdanbi <l26tempo@naver.com>
2022-10-31 12:05:50 +0900

    1031

--- telechips/doc/m011.php
+++ telechips/doc/m011.php
@@ -2,21 +2,25 @@
 include_once('../../../common.php');
 $page_num = 1;
 $page_title = "COMPANY";
-include_once(G5_THEME_PATH.'/head.php');
+include_once(G5_THEME_PATH . '/head.php');
 ?>
 
 
-<main>
-<section class="main_visual">
-    <div class="main_slide">
-        <div>s01</div>
-        <div>s02</div>
-        <div>s03</div>
-    </div>
-</section>
-</main>
-
+<div class="sub_content">
+  <h3>CEO 인사말</h3>
+  <p class="title">
+    <?= $as_slogan ?>
+  </p>
+  <figure>
+    <img src="../img/img_sector2_r.jpg" alt="">
+  </figure>
+  <strong>
+    Create a Foundation for Seeing and Listening to Enrich Human Life
+  </strong>
+  <p>Telechips는 Automotive와 Smart Home 분야의 Connectivity & Multimedia 솔루션을 제공하고 있는 글로벌 팹리스 반도체 기업입니다.</p>
+  <p>Telechips는 ‘꿈과 재능을 지닌 유능한 인재들이 모여, 보고 듣는 즐거움을 제공하는 세상의 모든 정보를 가장 편리하게 연결하고, 가장 똑똑하게 처리한다’ 는 비전 하에 미래 디지털 라이프 기술을 창조·선도 하고 있습니다.</p>
+</div>
 
 
 <?php
-include_once(G5_THEME_PATH.'/tail.php');
+include_once(G5_THEME_PATH . '/tail.php');
